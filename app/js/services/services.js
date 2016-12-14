93ff9487dabb6bc0bd6759651fe4da128997e562
proofreadinguk <mbax9et3@Lefteris-MacBook-Pro.local>
2016-12-14 20:33:24 +0200

    Service.js comment

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -1,5 +1,6 @@
 var jsonService = angular.module('jsonService', ['ngResource'])
     .factory('jsonService', function ($resource) {
+    	// This url might need changing for deployment
  			return $resource('../../data/ELSIO-Graph-Example.json', {}, {
          	   query: {method:'GET'}
         });
